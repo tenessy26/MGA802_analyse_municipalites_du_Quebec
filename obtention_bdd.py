e1b2586673c90d3b06e6753c16b356b0902a68ca
tenessy_de-faria <tenessy.de-faria@knap.fr>
2024-06-05 16:50:09 -0400

    Création d'une fonction générant la DataFrame

--- obtention_bdd.py
+++ obtention_bdd.py
@@ -1,10 +1,11 @@
 import pandas as pd
 
-df_cleaned = pd.read_csv('Census_2016_2021.csv') # On charge le fichier
-municipalites = df_cleaned[df_cleaned['Type'] == 'MÉ'] # Filtrage des éléments de type MÉ
+df = pd.read_csv('Census_2016_2021.csv') # On charge le fichier
 
-nombre_municipalites = municipalites.shape[0]
-print(f'Le nombre de municipalités est : {nombre_municipalites}')
+def afficher_dataframe():
+    municipalites = df[df['Type'] == 'MÉ'].reset_index(drop=True)  # Filtrage des éléments de type MÉ
+    print(f'Le nombre de municipalités est : {municipalites.shape[0]}\n')
+    print(municipalites.head())
+    return municipalites
 
-print(municipalites.head())
-print(municipalites.head())
+afficher_dataframe()
